e655380aa15ac9cece766e305b9915ec15705162
Bernardo Pineda <_@bernardopineda.mx>
2015-08-25 00:44:34 -0500

    Implements IAttribute Methods are for now just empty

---
+++ src/CloudDevStudio/EAV/Attribute.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bernardopineda
+ * Date: 8/25/15
+ * Time: 12:35 AM
+ */
+
+namespace CloudDevStudio\EAV;
+
+
+class Attribute implements IAttribute
+{
+    /**
+     *
+     */
+    public function getMeta()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function getValue()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function getType()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function deleteValue()
+    {
+
+    }
+
+    /**
+     * @param $new_value
+     */
+    public function updateValue($new_value)
+    {
+
+    }
+}
