d362caa2dd50a440611a4189b733ddfc5f474983
Bernardo Pineda <_@bernardopineda.mx>
2015-08-25 00:39:55 -0500

    Implements IEntity Methods are for now just empty

---
+++ src/CloudDevStudio/EAV/Entity.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bernardopineda
+ * Date: 8/25/15
+ * Time: 12:35 AM
+ */
+
+namespace CloudDevStudio\EAV;
+
+
+class Entity implements IEntity
+{
+
+    public function __construct()
+    {
+
+    }
+    /**
+     *
+     */
+    public function getBaseAttributes()
+    {
+
+    }
+
+    /**
+     * @param $attribute_name
+     */
+    public function hasAttribute($attribute_name)
+    {
+
+    }
+}
